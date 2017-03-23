44809c3492a8af7d50214f5e3cd9aba0f8ab6008
Oleg <mr.dvorezky@gmail.com>
2017-03-23 15:20:59 +0300

    create project

---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<?php
+require 'путь/PHPMailerAutoload.php';
+
+$mail = new PHPMailer();//создаем объект
+
+$body = file_get_contents('index.html'); //отправляется или тело письма с файлом или свойство Body 
+
+$mail->isSMTP(); //установка отправки писем через SMTP-сервер
+//далее настройки, описываемые в требования SMTP mail.ru
+$mail->CharSet = 'UTF-8';
+$mail->Host = 'smtp.mail.ru'; //устанавливаем хост
+$mail->SMTPAuth = true;//сервер требует аутентификации
+$mail->Username = 'dvorezky';//логин на почте
+$mail->Password = '5734em-5';//пароль от почты
+$mail->SMTPSecure = 'ssl'; //протокол шифрования
+$mail->Port = '465';
+
+$mail->From = 'dvorezky@mail.ru'; //от кого отправлено письмо
+$mail->FromName = 'Олег';
+$mail->addAddress('support@transkribator.ru', 'Петя');
+$mail->addAddress('mr.dvorezky@yandex.ru', 'Петя');
+$mail->MsgHTML($body);
+
+//Параметры письма
+$mail->Subject = 'Тема письма';
+// $mail->Body = 'Привет, мир!';
+
+if($mail->send()){
+	echo 'Письмо отправлено';
+}else{
+	echo 'Письмо не отправлено';
+	echo 'Ошибка: '.$mail->Errorinfo;
+}
+
+
+
+ ?>
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Как пользоваться скриптом
+
+1. Скачать библиотеку PHPMailer здесь: https://github.com/Synchro/PHPMailer
+2. Подключаем PHPMailerAutoload.php
+3. И далее по примеру здесь: http://phpmailer.worxware.com/index.php?pg=examplebmail
+4. SMTP для каждого почтовика свой. В поиск: smtp mail.ru или smtp google.com
+5. Все методы и свойства объекта $mail здесь: http://phpmailer.worxware.com/
